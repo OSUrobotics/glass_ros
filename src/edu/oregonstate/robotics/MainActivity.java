cac5743b77b8303ae7fde74284f5c24a86eb7fce
Dan Lazewatsky <dan@lazewatsky.com>
2013-08-30 15:53:05 -0500

    Added full IMU Broke all sensor code into its own class so it can be used as a library

--- src/edu/oregonstate/robotics/MainActivity.java
+++ src/edu/oregonstate/robotics/MainActivity.java
@@ -7,12 +7,7 @@ import edu.oregonstate.robotics.R;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,34 +18,12 @@ import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements SensorEventListener,
-		LocationListener {
+public class MainActivity extends Activity {
 	
 	private static final String LOG_TAG = "SensorTest";
-
 	private TextView text;
-
 	private TextView locationText;
-
-	private SensorManager mSensorManager;
-
-	private Sensor mOrientation, mAcceleration;
-
 	private LocationManager mLocationManager;
-	
-	Float azimuth_angle;
-
-	Float pitch_angle;
-	
-	Float roll_angle;
-	
-	Double networkLat;
-	
-	Double networkLon;
-	
-	Double gpsLat;
-	
-	Double gpsLon;
 	
 	RosSensorIO sensorIO;
 	
@@ -77,21 +50,10 @@ public class MainActivity extends Activity implements SensorEventListener,
 
 		setContentView(R.layout.activity_main);
 		
-		//removeBackgrounds(getWindow().getDecorView());
-
 		text = (TextView) findViewById(R.id.text);
 
 		locationText = (TextView) findViewById(R.id.location);
 
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-		for(Sensor sensor : sensors) {
-			Log.i(LOG_TAG, "Found sensor: " + sensor.getName());
-		}
-		
-		mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-		
-		mAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		
 		mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		List<String> providers = mLocationManager.getAllProviders();
@@ -99,93 +61,21 @@ public class MainActivity extends Activity implements SensorEventListener,
 			Log.i(LOG_TAG, "Found provider: " + provider);
 		}
 		
-		
-		// getting GPS status
-		boolean isGPSEnabled = mLocationManager
-				.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-		// getting network status
-		boolean isNetworkEnabled = mLocationManager
-				.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-		locationText.setText("isGPSEnabled:" + isGPSEnabled
-				+ "\nisNetworkEnabled:" + isNetworkEnabled);
-
-		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				0, 0, this);
-		//mLocationManager.requestLocationUpdates(
-		//		LocationManager.NETWORK_PROVIDER, 0, 0, this);
-		this.sensorIO = new RosSensorIO("10.68.0.112", 9999);
+		this.sensorIO = new RosSensorIO((SensorManager) getSystemService(Context.SENSOR_SERVICE), "192.168.0.158", 9999);
 		Thread clientThread = new Thread(this.sensorIO);
 		clientThread.start();
 	}
 
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// Do something here if sensor accuracy changes.
-		// You must implement this callback in your code.
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mSensorManager.registerListener(this, mOrientation,
-				SensorManager.SENSOR_DELAY_NORMAL);
-		mSensorManager.registerListener(this, mAcceleration,
-				SensorManager.SENSOR_DELAY_NORMAL);
-	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		mSensorManager.unregisterListener(this);
+//		mSensorManager.unregisterListener(this);
 	}
+
 
 	@Override
-	public void onSensorChanged(SensorEvent event) {
-		if(Sensor.TYPE_ORIENTATION == event.sensor.getType()) {
-			float azimuth_angle = event.values[0];
-			float pitch_angle = event.values[1];
-			float roll_angle = event.values[2];
-			// Do something with these orientation angles.
-			text.setText(
-				"roll, pitch, yaw = \n"
-				+ roll_angle  + "\n"
-				+ pitch_angle + "\n"
-				+ azimuth_angle
-			);
-			this.sensorIO.sendOrientation(roll_angle, pitch_angle, azimuth_angle);
-		} else if(Sensor.TYPE_ACCELEROMETER == event.sensor.getType()) {
-			float az = event.values[0];
-			float ay = event.values[1];
-			float ax = event.values[2];
-
-			text.setText(
-				"ax, ay, az = \n"
-				+ ax + "\n"
-				+ ay + "\n"
-				+ az
-			);
-			this.sensorIO.sendAcceleration(ax, ay, az);
-		}
+	protected void onResume() {
+		super.onResume();
 	}
-
-	@Override
-	public void onLocationChanged(Location location) {
-		locationText.setText(location.getLatitude() + "\n"
-				+ location.getLongitude());
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-	}
-
 }
--- src/edu/oregonstate/robotics/RosSensorIO.java
+++ src/edu/oregonstate/robotics/RosSensorIO.java
@@ -3,18 +3,41 @@ package edu.oregonstate.robotics;
 import java.net.*;
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.List;
+
 import android.util.Log;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class RosSensorIO implements Runnable {
+public class RosSensorIO implements Runnable, SensorEventListener {
 	Socket client;
 	DataOutputStream outStream;
 	String serverName;
-	int port;	
+	int port;
+	private static final String LOG_TAG = "SensorTest";
+	private SensorManager mSensorManager;
+
+	private Sensor mOrientation, mAcceleration, mIlluminance, mProximity, mGyro, mRotation;
+
 	
-	RosSensorIO(String serverName, int port) {
+	RosSensorIO(SensorManager sensorManager, String serverName, int port) {
 		this.serverName = serverName;
 		this.port = port;
+		this.mSensorManager = sensorManager;
+		
+		List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+		for(Sensor sensor : sensors) {
+			Log.i(LOG_TAG, "Found sensor: " + sensor.getName());
+		}
+		
+		mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+		mAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		mRotation = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+		mIlluminance = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+		mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 	}
 	
 	private synchronized void sendPrefixedTriplet(int prefix, float a, float b, float c) {
@@ -42,11 +65,21 @@ public class RosSensorIO implements Runnable {
 	public void sendAcceleration(float ax, float ay, float az) {
 		this.sendPrefixedTriplet(Sensor.TYPE_ACCELEROMETER, ax, ay, az);
 	}
+	public void sendGyro(float vx, float vy, float vz) {
+		this.sendPrefixedTriplet(Sensor.TYPE_GYROSCOPE, vx, vy, vz);
+	}
+	public void sendRotation(float ax, float ay, float az) {
+		this.sendPrefixedTriplet(Sensor.TYPE_ROTATION_VECTOR, ax, ay, az);
+	}
 	
 	public void sendOrientation(float roll, float pitch, float yaw) {
 //		this.sendPrefixedTriplet(Sensor.TYPE_ORIENTATION, -roll, pitch, -yaw+180);
-		this.sendPrefixedTriplet(Sensor.TYPE_ORIENTATION, -roll, pitch+90, -yaw);
+		this.sendPrefixedTriplet(Sensor.TYPE_ORIENTATION, -roll, pitch+90, -yaw-70);
 
+	}
+	
+	public void sendLight(float l) {
+		this.sendPrefixedTriplet(Sensor.TYPE_LIGHT, l, 0, 0);
 	}
 
 	public void run() {
@@ -59,6 +92,49 @@ public class RosSensorIO implements Runnable {
 		} catch (IOException e) {
 			Log.e("RosSensorIO", "IOException", e);
 		}
+		mSensorManager.registerListener(this, mOrientation,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		//mSensorManager.registerListener(this, mIlluminance,
+		//		SensorManager.SENSOR_DELAY_NORMAL);
+		//mSensorManager.registerListener(this, mProximity,
+		//		SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mAcceleration,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mGyro,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mRotation,
+				SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
+	
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(Sensor.TYPE_ORIENTATION == event.sensor.getType()) {
+			float azimuth_angle = event.values[0];
+			float pitch_angle = event.values[1];
+			float roll_angle = event.values[2];
+			// Do something with these orientation angles.
+			this.sendOrientation(roll_angle, pitch_angle, azimuth_angle);
+		} else if(Sensor.TYPE_ACCELEROMETER == event.sensor.getType()) {
+			float az = event.values[0];
+			float ay = event.values[1];
+			float ax = event.values[2];
+
+			this.sendAcceleration(ax, ay, az);
+		} else if(Sensor.TYPE_GYROSCOPE == event.sensor.getType()) {
+			this.sendGyro(event.values[0], event.values[1], event.values[2]);
+		} else if(Sensor.TYPE_ROTATION_VECTOR == event.sensor.getType()) {
+			this.sendRotation(event.values[0], event.values[1], event.values[2]);
+		} else if(Sensor.TYPE_LIGHT == event.sensor.getType()) {
+			this.sendLight(event.values[0]);
+		} else if(Sensor.TYPE_PROXIMITY == event.sensor.getType()) {
+//			Log.i(LOG_TAG, "Got proximity: " + event.values[0]);
+		}
+	}
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// Do something here if sensor accuracy changes.
+		// You must implement this callback in your code.
+	}
+	
 }
